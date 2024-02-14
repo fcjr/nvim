42901c9dc85c3d4ee881edac7f3957318cec5510
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2024-02-14 10:20:02 -0500

    fix spacing

--- lua/fcjr/set.lua
+++ lua/fcjr/set.lua
@@ -3,7 +3,8 @@ vim.opt.guicursor = ''
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
